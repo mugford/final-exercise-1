3a834750071da26afa4960dfba05ce439f977b65
Mugford <sam.mugford@jic.ac.uk>
2014-02-04 08:43:04 -0800

    trimmed2

--- print_col_names.py
+++ print_col_names.py
@@ -1,14 +1,10 @@
 import pandas
 data = pandas.read_csv("weather_data/weather_year.csv")
 col_names = data.columns.values.tolist()
-print "\n"
-print col_names
 col_names2=[]
 for i in col_names[0:len(col_names)]:
 	i=str(i).strip()
 	col_names2.append(i)
-print "\n\n"
-print col_names2
 outfile=open("columnNames", "w")
 outfile.write('\n'.join(col_names2))
 
